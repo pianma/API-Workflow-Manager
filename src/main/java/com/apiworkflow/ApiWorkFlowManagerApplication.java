9f58bebc0fb3db3c2196640cc5ad27613b7af442
kimgeonwoo <ikkw2040@naver.com>
2024-12-26 09:17:47 +0900

    initial commit

---
+++ src/main/java/com/apiworkflow/ApiWorkFlowManagerApplication.java
@@ -0,0 +1,13 @@
+package com.apiworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWorkFlowManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiWorkFlowManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apiworkflow/ApiWorkFlowManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWorkFlowManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
